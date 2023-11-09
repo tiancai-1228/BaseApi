a08c35d859bb8dd811f3e543ca2bbf85fe62f64b
tiancai-1228 <sky0975657713@gmail.com>
2023-11-10 01:13:29 +0800

    add useContainer

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import 'reflect-metadata';
 import Koa from 'koa';
-import { useKoaServer } from 'routing-controllers';
+import { useContainer, useKoaServer } from 'routing-controllers';
 import KoaLogger from 'koa-logger';
+import Container from 'typedi';
 
 const main = async () => {
     const app = new Koa();
     app.use(KoaLogger());
     app.listen(3000);
 
+    useContainer(Container);
     useKoaServer(app, {
         defaultErrorHandler: false,
         controllers: [__dirname + '/**/*.controller.{ts,js}'],
+        middlewares: [__dirname + '/**/*.{middleware,interceptor}.{ts,js}'],
     });
 };
 
--- src/module/login/controllers/auth.controller.ts
+++ src/module/login/controllers/auth.controller.ts
@@ -1,17 +1,20 @@
 import Koa from 'koa';
-import Container, { Inject } from 'typedi';
+import { Inject, Service } from 'typedi';
 import { JsonController, Post, Ctx, Body } from 'routing-controllers';
 import { LoginParams, Me } from '../types/auth.type';
 import { AuthService } from '../services/auth.service';
 
 @JsonController('/')
+@Service()
 export class AuthController {
+    @Inject()
+    private authService: AuthService;
+
     @Post('/login')
     async Login(
         @Ctx() ctx: Koa.Context,
         @Body() params: LoginParams,
     ): Promise<Me> {
-        const authService = Container.get(AuthService);
-        return authService.login(params);
+        return await this.authService.login(params);
     }
 }
